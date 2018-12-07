19982fbdb4c0143d29a30092ab6dcb0f74717960
Jordan Dale <jordan.dale2@gmail.com>
2018-12-07 14:10:54 -0500

    Created models

---
+++ main/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Post(models.Model):
+    created_at = models.DateTimeField()
+    title = models.CharField(max_length=255)
+    picture = models.CharField(max_length=400)
+    content = models.CharField(max_length=255)
+    site_url = models.CharField(max_length=400)
+    vote_total = models.IntegerField()
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    created_at = models.DateTimeField()
+    content = models.CharField(max_length=255)
+    vote_total = models.IntegerField()
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+
+    def __str__(self):
+        return self.content
