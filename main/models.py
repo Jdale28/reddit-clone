4c3473fee62a8b459465846ae16e1b2646493c80
Jordan Dale <jordan.dale2@gmail.com>
2018-12-07 15:35:15 -0500

    Added fixtures file

--- main/models.py
+++ main/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Post(models.Model):
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(null=True, blank=True)
     title = models.CharField(max_length=255)
     picture = models.CharField(max_length=400)
     content = models.CharField(max_length=255)
     site_url = models.CharField(max_length=400)
-    vote_total = models.IntegerField()
+    vote_total = models.IntegerField(null=True, blank=True)
     # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
@@ -14,9 +14,9 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(null=True, blank=True)
     content = models.CharField(max_length=255)
-    vote_total = models.IntegerField()
+    vote_total = models.IntegerField(null=True, blank=True)
     # user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
